08529b2862c9c6d2c35f849939ca872127849caf
Aman <amsingh714@gmail.com>
2019-04-02 13:23:02 -0400

    added session store

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const configuredKnex = require("../database/dbConfig");
+
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 
 const server = express();
 
@@ -14,21 +17,28 @@ const sessionConfig = {
   cookie: {
     maxAge: 1000 * 60 * 10, // 10 minutes
     secure: false, // only send the cookie for https, should be true in production!!!
-    httpOnly: true, // js cant access this. false means JS can access the cookie
+    httpOnly: true // js cant access this. false means JS can access the cookie
   },
-    resave: false, // Forces the session to be saved back to the session store, even if the session was never modified during the request. Default is true.
-    saveUninitialized: false, // docs have this as true. Forces a session that is "uninitialized" to be saved to the store. Choosing false is useful for implementing login sessions, reducing server storage usage, or complying with laws that require permission before setting a cookie.
-}
+  resave: false, // Forces the session to be saved back to the session store, even if the session was never modified during the request. Default is true.
+  saveUninitialized: false, // docs have this as true. Forces a session that is "uninitialized" to be saved to the store. Choosing false is useful for implementing login sessions, reducing server storage usage, or complying with laws that require permission before setting a cookie.
+  store: new KnexSessionStore({
+    knex: configuredKnex, // asks knex if we already have this file
+    tablename: "sessions", // used to save session if server restarts
+    sidfieldname: "sid", // data inside of your database
+    createtable: true, // creates this table if it does not exist
+    clearInterval: 1000 * 60 * 30 // delete expired sessions
+  })
+};
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 server.use(session(sessionConfig));
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send("It's alive!");
 });
 
